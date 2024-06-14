f3d28f86b2c05d8559135c076734fb32d99b54ca
JeanHerlin <ramangajeanherlin@gmailcom>
2024-06-14 22:14:11 +0300

    services pages

--- app/main/service/livraison-mod/page.jsx
+++ app/main/service/livraison-mod/page.jsx
@@ -1,7 +1,7 @@
 /** @format */
 
 import React from "react";
-import "./style.css";
+import "../style.css";
 import { Search } from "lucide-react";
 import Services from "@/components/service_pages/services";
 import Livraison from "@/components/service_pages/livraison";
@@ -10,25 +10,6 @@ import PayMode from "@/components/service_pages/pay-mode";
 const Service = () => {
   return (
     <div className="service">
-      <div className="top">
-        <span>
-          <input type="text" name="search" id="search" />
-          <div className="flex items-center gap-2 pl-2 pr-2">
-            {" "}
-            <Search />
-          </div>
-        </span>
-        <span>
-          <div>
-            <select name="short" id="short">
-              <option value="1">Copie</option>
-              <option value="1">CIN</option>
-              <option value="1">Copie</option>
-              <option value="1">Copie</option>
-            </select>
-          </div>
-        </span>
-      </div>
       <div className="bottom page_content">
           <h1><div className="page_name"><div><span>Service</span></div></div> CitizenConnect</h1>
           <p>Lorem ipsum dolor sit amet consectetur. In blandit nec pharetra lorem nulla consectetur enim maecenas mauris. Aliquam</p>
@@ -38,14 +19,14 @@ const Service = () => {
               <div>
                 <span>Pages</span>
                 <div>
-                  <small>01 / 03</small>
+                  <small>02 / 03</small>
                   <div className="progress_container">
-                    <div className="progress" style={{"width":`${100/3}%`}}></div>
+                    <div className="progress" style={{"width":`${100/2}%`}}></div>
                   </div>
                 </div>
               </div>
             </div>
-            <Services/>
+            <Livraison/>
          </div>
       </div>
     </div>
---
+++ components/service.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Service = () => {
+  return (
+    
+    <div className="service-content">
+    <div className="top">CIN</div>
+    <div className="bottom">
+        <div className="left">
+            <div>
+                <span>Nouveau</span>
+                <span>3000ar</span>
+            </div>
+            <div>
+                <span>Nouveau</span>
+                <span>3000ar</span>
+            </div>
+            <div>
+                <span>Nouveau</span>
+                <span>3000ar</span>
+            </div>
+        </div>
+        <div className="right">
+            <button>Proceder</button>
+        </div>
+    </div>
+</div>
+  )
+}
+
+export default Service
