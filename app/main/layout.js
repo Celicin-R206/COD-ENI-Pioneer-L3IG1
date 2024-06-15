116ce87f9492b7537288b4fbc83eff297444922e
Celicin-R206 <rahandrimiray@gmail.com>
2024-06-15 03:19:11 +0300

    after bug css

--- app/main/layout.js
+++ app/main/layout.js
@@ -1,7 +1,6 @@
 /** @format */
 import "./style.css";
 import Header_main from "@/components/header_main/header_main";
-import "../globals.css";
 
 export const metadata = {
   title: "Create Next App",
